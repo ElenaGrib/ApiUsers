1437bb663f89d68cf4d507532b0361aa0383fbed
ElenaGrib <elenagrib27@gmail.com>
2022-08-13 18:57:00 +0300

    Initial commit

---
+++ src/test/java/com/grib/api/users/photoappapiusers/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grib.api.users.photoappapiusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
