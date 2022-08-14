aa0c1750a2e2592f863c0797fea328c043c53b59
ElenaGrib <elenagrib27@gmail.com>
2022-08-14 15:07:54 +0300

    9. ModelMapper

--- src/main/java/com/grib/api/users/photoappapiusers/service/impl/UserServiceImpl.java
+++ src/main/java/com/grib/api/users/photoappapiusers/service/impl/UserServiceImpl.java
@@ -1,7 +1,9 @@
 package com.grib.api.users.photoappapiusers.service.impl;
 
+import com.grib.api.users.photoappapiusers.data.UserEntity;
 import com.grib.api.users.photoappapiusers.dto.UserDto;
 import com.grib.api.users.photoappapiusers.service.UserService;
+import org.modelmapper.ModelMapper;
 
 import java.util.UUID;
 
@@ -9,6 +11,9 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
+
+        ModelMapper modelMapper = new ModelMapper();
+        UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
         return null;
     }
 }
