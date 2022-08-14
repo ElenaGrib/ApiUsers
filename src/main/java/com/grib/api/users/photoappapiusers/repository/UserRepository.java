36dd10c51da919d8caa2852b8a10e80ac96b60f3
ElenaGrib <elenagrib27@gmail.com>
2022-08-14 15:11:00 +0300

    10. ModelMapper + Spring - DI

--- src/main/java/com/grib/api/users/photoappapiusers/repository/UserRepository.java
+++ src/main/java/com/grib/api/users/photoappapiusers/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.grib.api.users.photoappapiusers.repository;
 
+import com.grib.api.users.photoappapiusers.data.UserEntity;
 import org.apache.catalina.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
 }
--- src/main/java/com/grib/api/users/photoappapiusers/service/impl/UserServiceImpl.java
+++ src/main/java/com/grib/api/users/photoappapiusers/service/impl/UserServiceImpl.java
@@ -2,18 +2,34 @@ package com.grib.api.users.photoappapiusers.service.impl;
 
 import com.grib.api.users.photoappapiusers.data.UserEntity;
 import com.grib.api.users.photoappapiusers.dto.UserDto;
+import com.grib.api.users.photoappapiusers.repository.UserRepository;
 import com.grib.api.users.photoappapiusers.service.UserService;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDto createUser(UserDto userDto) {
         userDto.setUserId(UUID.randomUUID().toString());
 
         ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         UserEntity userEntity = modelMapper.map(userDto, UserEntity.class);
+
+        userRepository.save(userEntity);
         return null;
     }
 }
