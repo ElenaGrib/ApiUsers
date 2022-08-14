346c744d19dd96f05f922ae6a1df7dc074348f29
ElenaGrib <elenagrib27@gmail.com>
2022-08-14 17:03:06 +0300

    15. AuthenticationFilter

--- src/main/java/com/grib/api/users/photoappapiusers/security/AuthenticationFilter.java
+++ src/main/java/com/grib/api/users/photoappapiusers/security/AuthenticationFilter.java
@@ -1,10 +1,16 @@
 package com.grib.api.users.photoappapiusers.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.grib.api.users.photoappapiusers.dto.UserDto;
+import com.grib.api.users.photoappapiusers.service.UserService;
 import com.grib.api.users.photoappapiusers.ui.model.LoginRequestModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -15,6 +21,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private UserService userService;
+    private Environment environment;
+
+    @Autowired
+    public AuthenticationFilter(AuthenticationManager authenticationManager, UserService userService, Environment environment) {
+        super.setAuthenticationManager(authenticationManager);
+        this.userService = userService;
+        this.environment = environment;
+    }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
@@ -39,8 +55,9 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
                                             HttpServletResponse response,
                                             FilterChain chain,
                                             Authentication authentication) throws IOException, ServletException {
+        String userName = ((User) authentication.getPrincipal()).getUsername();
+        UserDto userDto = userService.getUserDetailsByEmail(userName);
+
 
     }
-
-
 }
