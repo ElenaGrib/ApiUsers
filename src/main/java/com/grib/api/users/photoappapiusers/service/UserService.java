9530c112e030bcc51979df44d1429f1c3cb6eb72
ElenaGrib <elenagrib27@gmail.com>
2022-08-14 14:33:25 +0300

    5. UserService

---
+++ src/main/java/com/grib/api/users/photoappapiusers/service/UserService.java
@@ -0,0 +1,7 @@
+package com.grib.api.users.photoappapiusers.service;
+
+import com.grib.api.users.photoappapiusers.dto.UserDto;
+
+public interface UserService {
+    UserDto createUser(UserDto userDto);
+}
---
+++ src/main/java/com/grib/api/users/photoappapiusers/service/impl/UserServiceImpl.java
@@ -0,0 +1,11 @@
+package com.grib.api.users.photoappapiusers.service.impl;
+
+import com.grib.api.users.photoappapiusers.dto.UserDto;
+import com.grib.api.users.photoappapiusers.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        return null;
+    }
+}
