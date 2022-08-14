835a099d046ff13e9b7e374ab107bd6f65d83e6b
ElenaGrib <elenagrib27@gmail.com>
2022-08-14 14:37:53 +0300

    7. UUID

--- src/main/java/com/grib/api/users/photoappapiusers/service/impl/UserServiceImpl.java
+++ src/main/java/com/grib/api/users/photoappapiusers/service/impl/UserServiceImpl.java
@@ -3,9 +3,12 @@ package com.grib.api.users.photoappapiusers.service.impl;
 import com.grib.api.users.photoappapiusers.dto.UserDto;
 import com.grib.api.users.photoappapiusers.service.UserService;
 
+import java.util.UUID;
+
 public class UserServiceImpl implements UserService {
     @Override
     public UserDto createUser(UserDto userDto) {
+        userDto.setUserId(UUID.randomUUID().toString());
         return null;
     }
 }
